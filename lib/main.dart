902fe8998fa9d53fe13813e15e19bb77fdaa9ec1
prajwalathreya <pathre2@uic.edu>
2023-10-30 17:52:13 -0500

    1. added image file 2. updates main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -11,10 +12,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
+    
+    return ChangeNotifierProvider(  
       create: (context) => MyAppState(),
       child: MaterialApp(
-        title: 'Namer App',
+        title: 'Bike Navigation System',
         theme: ThemeData(
           useMaterial3: true,
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
@@ -27,22 +29,64 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+  void getNext(){
+    current = WordPair.random();
+    notifyListeners();
+  }
+
 }
 
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+    final screenSize = MediaQuery.of(context).size;
+    final isSmallScreen = screenSize.width < 600; // Adjust the threshold as needed
 
     return Scaffold(
-      body: Column(
-        children: [
-          Text("HCI : Final Semester Project - Real Time Navigation system for bikers in Chicago!", textAlign: TextAlign.center,),
-          Text(appState.current.asLowerCase),
-          ElevatedButton(onPressed:(){
-            print("Hello you pressed this button");
-          }, child: Text("Next",textAlign: TextAlign.center,))
-        ],
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/black-grey-chicago-city-area-background-map-vector-37821140.jpg'), // Replace with your image path
+            fit: BoxFit.cover, // Adjust the fit to your preference
+          ),
+        ),
+        child: Center(
+          child: Column(
+            // SizedBox(height: 30),
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Align(
+                alignment: Alignment.topCenter,
+                
+                child: Text(
+                  "Bike Navigation System",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white, // Text color on top of the image
+                    fontSize: isSmallScreen ? 20.0 : 24.0, // Adjust font size
+                  )
+                ),
+                
+              ),
+              // spacer(),
+              Text(
+                appState.current.asPascalCase,
+                style: TextStyle(
+                  color: Colors.white, // Text color on top of the image
+                  fontSize: isSmallScreen ? 16.0 : 20.0, // Adjust font size
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  print("Hello, you pressed this button");
+                },
+                child: Text("Start Navigation", textAlign: TextAlign.center),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
