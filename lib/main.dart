70af84c6e1197f38efd217ab151b70aeb4b78da8
prajwalathreya <pathre2@uic.edu>
2023-11-01 19:48:09 -0500

    SOS button added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,96 @@
 
+// import 'package:english_words/english_words.dart';
+// import 'package:flutter/material.dart';
+// import 'package:provider/provider.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+    
+//     return ChangeNotifierProvider(  
+//       create: (context) => MyAppState(),
+//       child: MaterialApp(
+//         title: 'Bike Navigation System',
+//         theme: ThemeData(
+//           useMaterial3: true,
+//           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+//         ),
+//         home: MyHomePage(),
+//       ),
+//     );
+//   }
+// }
+
+// class MyAppState extends ChangeNotifier {
+//   var current = WordPair.random();
+//   void getNext(){
+//     current = WordPair.random();
+//     notifyListeners();
+//   }
+
+// }
+
+// class MyHomePage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     var appState = context.watch<MyAppState>();
+//     final screenSize = MediaQuery.of(context).size;
+//     final isSmallScreen = screenSize.width < 600; // Adjust the threshold as needed
+
+//     return Scaffold(
+//       body: Container(
+//         decoration: BoxDecoration(
+//           image: DecorationImage(
+//             image: AssetImage('assets/black-grey-chicago-city-area-background-map-vector-37821140.jpg'), // Replace with your image path
+//             fit: BoxFit.cover, // Adjust the fit to your preference
+//           ),
+//         ),
+//         child: Center(
+//           child: Column(
+//             // SizedBox(height: 30),
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             crossAxisAlignment: CrossAxisAlignment.center,
+//             children: [
+//               Align(
+//                 alignment: Alignment.topCenter,
+                
+//                 child: Text(
+//                   "Bike Navigation System",
+//                   textAlign: TextAlign.center,
+//                   style: TextStyle(
+//                     color: Colors.white, // Text color on top of the image
+//                     fontSize: isSmallScreen ? 20.0 : 24.0, // Adjust font size
+//                   )
+//                 ),
+                
+//               ),
+//               // spacer(),
+//               Text(
+//                 appState.current.asPascalCase,
+//                 style: TextStyle(
+//                   color: Colors.white, // Text color on top of the image
+//                   fontSize: isSmallScreen ? 16.0 : 20.0, // Adjust font size
+//                 ),
+//               ),
+//               ElevatedButton(
+//                 onPressed: () {
+//                   print("Hello, you pressed this button");
+//                 },
+//                 child: Text("Start Navigation", textAlign: TextAlign.center),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -8,12 +100,11 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key});
 
   @override
   Widget build(BuildContext context) {
-    
-    return ChangeNotifierProvider(  
+    return ChangeNotifierProvider(
       create: (context) => MyAppState(),
       child: MaterialApp(
         title: 'Bike Navigation System',
@@ -29,11 +120,10 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
-  void getNext(){
+  void getNext() {
     current = WordPair.random();
     notifyListeners();
   }
-
 }
 
 class MyHomePage extends StatelessWidget {
@@ -44,49 +134,69 @@ class MyHomePage extends StatelessWidget {
     final isSmallScreen = screenSize.width < 600; // Adjust the threshold as needed
 
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/black-grey-chicago-city-area-background-map-vector-37821140.jpg'), // Replace with your image path
-            fit: BoxFit.cover, // Adjust the fit to your preference
-          ),
-        ),
-        child: Center(
-          child: Column(
-            // SizedBox(height: 30),
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Align(
-                alignment: Alignment.topCenter,
-                
-                child: Text(
-                  "Bike Navigation System",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.white, // Text color on top of the image
-                    fontSize: isSmallScreen ? 20.0 : 24.0, // Adjust font size
-                  )
-                ),
-                
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/black-grey-chicago-city-area-background-map-vector-37821140.jpg'),
+                fit: BoxFit.cover,
               ),
-              // spacer(),
-              Text(
-                appState.current.asPascalCase,
+            ),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Align(
+                    alignment: Alignment.topCenter,
+                    child: Text(
+                      "Bike Navigation System",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: isSmallScreen ? 20.0 : 24.0,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    appState.current.asPascalCase,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: isSmallScreen ? 16.0 : 20.0,
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      print("Hello, you pressed this button");
+                    },
+                    child: Text("Start Navigation", textAlign: TextAlign.center),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            top: 20.0,
+            right: 20.0,
+            child: ElevatedButton(
+              onPressed: () {
+                // Add your logic for the circular button here
+              },
+              style: ElevatedButton.styleFrom(
+                shape: CircleBorder(),
+                padding: EdgeInsets.all(25),
+                primary: Colors.red,
+              ),
+              child: Text(
+                'SOS',
                 style: TextStyle(
-                  color: Colors.white, // Text color on top of the image
-                  fontSize: isSmallScreen ? 16.0 : 20.0, // Adjust font size
+                  color: Colors.white,
                 ),
               ),
-              ElevatedButton(
-                onPressed: () {
-                  print("Hello, you pressed this button");
-                },
-                child: Text("Start Navigation", textAlign: TextAlign.center),
-              ),
-            ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
